1b6fd109166f124a1b441482e49776af9b595961
Nutella176 <121295171+Nutella176@users.noreply.github.com>
2023-03-06 10:29:43 +0000

    Add files via upload

---
+++ hello_world.py
@@ -0,0 +1,11 @@
+#Ask user to input their name
+#Print name
+name = input("Please enter your name: ")
+print(name)
+
+#Ask user to input their age
+#Print age
+#Print "Hello World!"
+age = input("Please enter your age: ")
+print(age)
+print("Hello World!")
---
+++ menu.py
@@ -0,0 +1,12 @@
+#Ask user to input 3 items from menu
+#Print "It's ordered! Your order consists of"
+#Print the 3 items separately
+
+
+item1 = input("Please enter your first favourite food from the menu: ")
+item2 = input("Please enter your second favourite food from the menu: ")
+item3 = input("Please enter your third favourite food from the menu: ")
+print("It's ordered! Your order consists of: ")
+print(item1)
+print(item2)
+print(item3)
